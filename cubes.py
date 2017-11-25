9b60d17bd3e3b97b350e32dcbc0ff747aa921090
Keith Crowder <keith@attackoftheserver.com>
2017-11-25 07:42:06 -0600

    Add files via upload

---
+++ cubes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#
+# date: 11.24.2017
+# by: keith crowder
+#
+# exercise 4-8
+#
+cubes = []
+for value in range(1,21):
+  cube = value**3
+  cubes.append(cube)
+
+print(cubes)
+#
+# end of program
+#
