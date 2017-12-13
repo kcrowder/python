585b110af909c4f0bea4ad90d56956259c2865b2
Keith Crowder <keith@attackoftheserver.com>
2017-12-12 20:19:44 -0600

    exercise 7-1

---
+++ rental_car.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#
+# date: 12.11.2017
+# by: keith crowder
+# name: rental_car.py
+#
+# exercise 7-1
+#
+message = input("What kind of rental car would you like? ")
+print("Let me if I can locate you an " + message + ".")
+#
+# end of program
+#
