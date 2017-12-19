f0e3db25901d162f201fdcb1f77d145c60cc6209
Keith Crowder <keith@attackoftheserver.com>
2017-12-19 12:03:20 -0600

    exercise 8-1

---
+++ display.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#
+# date: 12.17.2017
+# by: keith crowder
+# name: display.py
+#
+# exercise 8-1
+#
+def display_message():
+        print("Learning about how functions work. Yay!")
+
+display_message()
+#
+# end of program
+#
