6e5cc4c596d790939d5a8ef91e7db8a98445f91a
Keith Crowder <keith@attackoftheserver.com>
2017-12-19 12:06:42 -0600

    exercise 8-2

---
+++ fav_book.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#
+# date: 12.18.2017
+# by: keith crowder
+# name: fav_book.py
+#
+# exercise 8-2
+#
+def favorite_book():
+    book = input("Enter the name of your favorite book: ")
+    print("One of my favorite books of all time is " + book.title() + "!")
+
+favorite_book()
+#
+# end of program
+#
