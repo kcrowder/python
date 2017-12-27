ffef18a16c9a365108c22ce1f5848f86bc3c0e28
Keith Crowder <keith@attackoftheserver.com>
2017-12-27 07:34:46 -0600

    exercise 8-9

---
+++ magicians.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# date: 12.25.2017
+# by: keith crowder
+# name: magicians.py
+#
+# exercise 8-9
+#
+def show_magicians(magicians):
+    for magician in magicians:
+            print(magician.title())
+#
+magicians = ['bob', 'bill','brenda']
+show_magicians(magicians)
+#
+# end of program
+#
