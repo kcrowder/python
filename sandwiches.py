f5d8c0fa33d8a736bbf5cf72a4b15225c9973a5e
Keith Crowder <keith@attackoftheserver.com>
2017-12-27 07:35:57 -0600

    exercise 8-12

---
+++ sandwiches.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# date: 12.27.2017
+# by: keith crowder
+# name: sandwiches.py
+#
+# exercise 8-12
+#
+def make_sandwiches(*ingredients):
+    print(ingredients)
+#
+make_sandwiches('turkey','lettuce','tomato','pickles','mustard')
+make_sandwiches('roast beef','lettuce','tomato','onions','mayo')
+make_sandwiches('chicken','lettuce','tomato','avocado','mustard')
+#
+# end of program
+#
